e781925838e58a0ae4cb42184eb32063dceb16e5
Swapnil Shende <swapnilshende1998@gmail.com>
2024-07-08 14:14:24 +0530

    added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logoContainer">
+        <img
+          alt="logo"
+          src="https://img.freepik.com/premium-vector/online-food-app-icon-food-shop-location-logo-also-online-resturent-location-template_608547-155.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
