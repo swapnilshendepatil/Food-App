59f8fcc93c1a7633e5b144972df415074dba4b5a
Swapnil Shende <swapnilshende1998@gmail.com>
2024-07-19 18:40:21 +0530

    add css

--- src/components/RestroMenupage.jsx
+++ src/components/RestroMenupage.jsx
@@ -1,60 +1,91 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/CustomHooks/useRestaurantMenu";
-import './ResMenuPage.css'
+import "./ResMenuPage.css";
+import { CDN_URL } from "../utils/constants";
 
 const RestroMenupage = () => {
     const resId = useParams();
     const [resInfo, setResInfo] = useState([]);
     const [menu, setMenu] = useState([]);
-    const { resMenuList, isLoading } = useRestaurantMenu(resId)
-    const [open, setOpen] = useState(false)
+    const { resMenuList, isLoading } = useRestaurantMenu(resId);
+    const [openIndex, setOpenIndex] = useState(null);
+
+    const toggleOpen = (index) => {
+        setOpenIndex(openIndex === index ? null : index);
+    };
 
     useEffect(() => {
         if (resMenuList) {
             setResInfo(resMenuList?.data?.cards[2]?.card?.card?.info);
             setMenu(
-                resMenuList?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
-                    ?.card?.itemCards
+                resMenuList?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards
             );
         }
     }, [resMenuList]);
 
-    console.log(resMenuList?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards)
+    const category = menu.filter(
+        (c) =>
+            c?.card?.card?.["@type"] ===
+            "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+
+    console.log(category);
 
     if (isLoading) return <div>Loading....</div>;
 
     return (
-        <div>
-            <h2>{resInfo?.name}</h2>
-            <p>
+        <div className="menu-container">
+            <h2 className="restaurant-name">{resInfo?.name}</h2>
+            <p className="restaurant-info">
                 {resInfo?.cuisines ? resInfo.cuisines.join(",") : "N/A"}-
                 {resInfo?.costForTwoMessage}
             </p>
-            <span>{resInfo?.avgRating}</span>
-
-            <div className="menu-container">
-                <h3 onClick={() => setOpen(!open)} className="menu-title">
-                    {resMenuList?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.title}
-                </h3>
-                {open && (
-                    <div className="menu-list">
-                        {menu.map((res) => (
-                            <li key={res?.card?.info?.id} className="menu-item">
-                                {res?.card?.info?.name} - {res?.card?.info?.price / 100}
-                            </li>
-                        ))}
+            <span className="restaurant-rating">{resInfo?.avgRating}</span>
+            <div className="accordion-container">
+                {category.map((data, index) => (
+                    <div key={index} className="accordion-item">
+                        <h3 className="accordion-title" onClick={() => toggleOpen(index)}>
+                            {data?.card?.card?.title}
+                        </h3>
+                        {openIndex === index && (
+                            <ul className="accordion-content">
+                                {data?.card?.card?.itemCards.map((resList, resIndex) => (
+                                    <div
+                                        key={resIndex}
+                                        style={{
+                                            display: "flex",
+                                            flexDirection: "row",
+                                            justifyContent: "space-between",
+                                        }}
+                                    >
+                                        <li className="accordion-list-item">
+                                            {resList?.card?.info?.name}
+                                            <span> ₹{resList?.card?.info?.price / 100}</span>
+                                            <span style={{ fontSize: "small", width: "450px" }}>
+                                                {resList?.card?.info?.description}
+                                            </span>
+                                        </li>
+                                        <div
+                                            style={{
+                                                display: "flex",
+                                                flexDirection: "column",
+                                                padding: "10px",
+                                            }}
+                                        >
+                                            <img
+                                                style={{ width: "90px", height: "90px" }}
+                                                src={CDN_URL + resList?.card?.info?.imageId}
+                                            />
+                                            <button className="cart-btn">Add</button>
+                                        </div>
+                                    </div>
+                                ))}
+                            </ul>
+                        )}
                     </div>
-                )}
+                ))}
             </div>
-            {/* {resMenuList?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.map((resmenuList, index) => (
-                <div key={index}>
-                    <h3> {resmenuList?.card?.card?.title} </h3>
-                    {resmenuList?.card?.card?.itemCards.map((menu) => (
-                        <span>{menu?.card?.info?.name}</span>
-                    ))}
-                </div>
-            ))} */}
         </div>
     );
 };
